2800bd8c773381e08b625a6c134aaf2bd64bdadc
tom hikari <tomprescott1565@gmail.com>
2023-06-18 17:42:42 +0100

    feature/rootReducer

--- src/utils/rootReducer.ts
+++ src/utils/rootReducer.ts
@@ -1,8 +1,16 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import postReducer from "../redux/feedReducer";
+import feedReducer from "../redux/feedReducer";
+import authReducer from "@/redux/authReducer";
+import settingReducer from "@/redux/settingReducer";
+import taxonomyReducer from "@/redux/taxonomyReducer";
+import userReducer from "@/redux/userReducer";
 
 export const rootReducer = combineReducers({
-  post: postReducer,
+  feed: feedReducer,
+  user: userReducer,
+  taxonomy: taxonomyReducer,
+  setting: settingReducer,
+  auth: authReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
