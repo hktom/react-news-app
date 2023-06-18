9fa55f523e858f002c93fa9658269b959a5e6fa3
tom hikari <tomprescott1565@gmail.com>
2023-06-18 20:25:43 +0100

    feature/simpleLoading

--- src/pages/me/index.page.tsx
+++ src/pages/me/index.page.tsx
@@ -1,9 +1,11 @@
+import SimpleLoading from "@/components/SimpleLoading";
 import MainLayout from "@/layout/mainLayout";
 
 function Index() {
   return (
     <MainLayout>
       <h1>Me</h1>
+      <SimpleLoading />
     </MainLayout>
   );
 }
