11e4cdb21a025fcc1619a09175b8416440f85fa7
tom hikari <tomprescott1565@gmail.com>
2023-06-20 12:52:01 +0100

    remove saga

--- src/pages/me/history.page.tsx
+++ src/pages/me/history.page.tsx
@@ -2,19 +2,41 @@ import FeedEmpty from "@/components/FeedEmpty";
 import HomeTab from "@/components/HomeTab";
 import SimpleLoading from "@/components/SimpleLoading";
 import PageFeed from "@/components/homeTabPage/pageFeed";
+import { ArticleFields } from "@/helpers/graphqlField";
 import MainLayout from "@/layout/mainLayout";
+import { apolloQuery } from "@/utils/apollo";
 import { useAppDispatch, useAppSelector } from "@/utils/hooks";
+import { IArticle } from "@/utils/interface";
 import { IReducer } from "@/utils/rootReducer";
+import { useEffect, useState } from "react";
 
 function Index() {
   const state = useAppSelector((state: IReducer) => state);
   const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<IArticle[]>([]);
 
-  
+  const getHistories = async () => {
+    setLoading(true);
+    const res = await apolloQuery(`{
+        getArticleBy(key:"already_read", value:"1"){
+            ${ArticleFields}
+        }
+    }`);
+
+    if (res?.data?.getArticleBy) {
+      setData(res?.data?.getArticleBy);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getHistories();
+  }, []);
 
   return (
     <MainLayout title="History" description="Find all articles you have read">
-      <PageFeed feeds={state.feed.exploreFeed} loading={state.feed.loading} />
+      <PageFeed feeds={data} loading={loading} />
     </MainLayout>
   );
 }
--- src/utils/rootSaga.ts
+++
@@ -1,6 +0,0 @@
-import { fork, SimpleEffect } from 'redux-saga/effects';
-import { postSagas } from '../redux/saga';
-
-export function* rootSaga(): IterableIterator<SimpleEffect<'FORK'>> {
-  yield fork(postSagas);
-}
